0a7e07aa1392143a2c5cfffecc0ed2920115dca5
Zach H <zdhadgraft@gtcc.edu>
2016-06-21 16:52:13 -0400

    Added seeds for users.

--- twitter_clone/db/seeds.rb
+++ twitter_clone/db/seeds.rb
@@ -8,13 +8,27 @@
 
 users = [
   {
-    username: "syd"
+    username: "SloppyVinny",
+    first_name: "Vincent",
+    last_name: "Van Gogh",
+    location: "Auvers-sur-Oise, France",
+    avatar: 'https://upload.wikimedia.org/wikipedia/commons/a/aa/Vincent_van_Gogh_-_Self-portrait_with_grey_felt_hat_-_Google_Art_Project.jpg'
+
   },
   {
-    username: "gord"
+    username: "Pabs",
+    first_name: "Pablo",
+    last_name: "Picasso",
+    location: "Mougins, France",
+    avatar: 'https://upload.wikimedia.org/wikipedia/commons/b/b8/Portrait_de_Picasso%2C_1908.jpg'
   },
 ]
 
 users.each do |user|
-  User.create!(username: user[:username])
+  User.create!(
+    username:   user[:username],
+    first_name: user[:first_name],
+    last_name:  user[:last_name],
+    location:   user[:location]
+  )
 end
